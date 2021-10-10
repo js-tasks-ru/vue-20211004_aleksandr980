337f5fb19fb5da0810aa8e0275fc1f8d65780305
xzibit <kostirko3@mail.ru>
2021-10-10 14:50:47 +0300

    master Реализация счётчика

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,19 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Root = defineComponent({
+  el: '#app',
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    countInc: () => {
+      this.count++;
+    }
+  }
+});
+
+const app = createApp(Root);
+const vm = app.mount('#app');
